ed9c07ca66dc6a4e9a2bc56999934df2641d4431
jamesujeon <jamesujeon@gmail.com>
2019-12-05 18:20:55 +0900

    Codeforces 231A - Team 문제 풀이 리팩토링

--- codeforces/python 3/231A - Team.py
+++ codeforces/python 3/231A - Team.py
@@ -1,20 +1,21 @@
 # 문제 링크: http://codeforces.com/problemset/problem/231/A
 
-# n = int(input())
-# isSureList = [tuple(map(int, input().split())) for i in range(n)]
-
-# count = 0
-# for isSure in isSureList:
-#   if sum(isSure) >= 2:
-#     count += 1
-
-# print(count)
-
-
-# 다른 버전
 n = int(input())
 isSureList = [tuple(map(int, input().split())) for i in range(n)]
 
-count = sum(1 if sum(isSure) >= 2 else 0 for isSure in isSureList)
+count = 0
+for isSure in isSureList:
+  if sum(isSure) >= 2:
+    count += 1
 
 print(count)
+
+
+# 다른 버전
+
+# n = int(input())
+# isSureList = [tuple(map(int, input().split())) for i in range(n)]
+
+# count = sum(1 if sum(isSure) >= 2 else 0 for isSure in isSureList)
+
+# print(count)
