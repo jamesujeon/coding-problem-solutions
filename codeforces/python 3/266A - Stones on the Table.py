46b1e454005d215b507a5cfa6f56fdf50fbb44fe
jamesujeon <jamesujeon@gmail.com>
2019-12-09 18:12:40 +0900

    Codeforces 266A - Stones on the Table 문제 풀이

--- codeforces/python 3/266A - Stones on the Table.py
+++ codeforces/python 3/266A - Stones on the Table.py
@@ -2,12 +2,6 @@
 
 n, s = int(input()), input()
 
-# result = 0
-# prev_stone = ''
-# for stone in s:
-#   if prev_stone == stone:
-#     result += 1
-
 result = sum(prev_stone == stone for prev_stone, stone in zip(s[1:], s))
 
 print(result)
