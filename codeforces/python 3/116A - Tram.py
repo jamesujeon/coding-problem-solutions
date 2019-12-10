9685e6b1bdf1e64b68ad5365f8089a2ce6a906df
jamesujeon <jamesujeon@gmail.com>
2019-12-10 14:00:24 +0900

    Codeforces 116A - Tram 문제 풀이

--- codeforces/python 3/116A - Tram.py
+++ codeforces/python 3/116A - Tram.py
@@ -4,13 +4,6 @@ from functools import reduce
 
 passengers = [tuple(map(int, input().split())) for _ in range(int(input()))]
 
-# count = 0
-# capacity = 0
-# for a, b in passengers:
-#   count += b - a
-#   capacity = max(capacity, count)
-
 capacity = reduce(lambda x, y: (max(x[0], x[1] - y[0] + y[1]), x[1] - y[0] + y[1]), passengers, (0, 0))[0]
 
-print(passengers)
 print(capacity)
