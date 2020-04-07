f2b2f5e5ca60a7222fc407d81ec7ddb7cab19989
jamesujeon <jamesujeon@gmail.com>
2020-04-08 00:07:01 +0900

    백준 1316 - 그룹 단어 체커 문제 풀이 리팩토링 by Python 3

--- baekjoon/python 3/1316.py
+++ baekjoon/python 3/1316.py
@@ -2,17 +2,5 @@
 
 import sys
 
-def check(s):
-  i, l = 0, []
-  while i < len(s):
-    if s[i] in l:
-      return False
-    l.append(s[i])
-    j = i + 1
-    while j < len(s) and s[i] == s[j]:
-      j += 1
-    i = j
-  return True
-
 input()
-print(sum(check(s) for s in sys.stdin))
+print(sum(list(s) == sorted(s, key=s.find) for s in sys.stdin))
