cb9d73ad4d5b83ade7c2620dec2c7e45222be767
jamesujeon <jamesujeon@gmail.com>
2020-04-26 00:51:02 +0900

    백준 2698 - 인접한 비트의 개수 문제 풀이 리팩토링 by Python 3

--- baekjoon/python 3/2698.py
+++ baekjoon/python 3/2698.py
@@ -1,6 +1,7 @@
 # 문제 링크: https://www.acmicpc.net/problem/2698
 
 import sys
+input = sys.stdin.readline
 
 dp = [[[0] * 2 for _ in range(100)] for _ in range(101)]
 dp[1][0][0] = dp[1][0][1] = 1
@@ -9,6 +10,6 @@ for n in range(2, 101):
     dp[n][k][0] = dp[n - 1][k][0] + dp[n - 1][k][1]
     dp[n][k][1] = dp[n - 1][k][0] + (dp[n - 1][k - 1][1] if k > 0 else 0)
 
-for _ in range(int(sys.stdin.readline())):
-  n, k = map(int, sys.stdin.readline().split())
+for _ in range(int(input())):
+  n, k = map(int, input().split())
   print(dp[n][k][0] + dp[n][k][1])
