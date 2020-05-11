ff6d43a47583695e444f28136f53a4bec79c3640
jamesujeon <jamesujeon@gmail.com>
2020-05-11 23:35:06 +0900

    LeetCode 121 - Best Time to Buy and Sell Stock 문제 풀이 by Python 3

---
+++ leetcode/python 3/121.py
@@ -0,0 +1,13 @@
+# 문제 링크: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+
+        max_profit = 0
+        min_price = prices[0]
+        for i in range(1, len(prices)):
+            max_profit = max(prices[i] - min_price, max_profit)
+            min_price = min(prices[i], min_price)
+        return max_profit
