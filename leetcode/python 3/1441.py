b14a06b3d8bf72adb294cb2b6e983f7db35bd5bd
jamesujeon <jamesujeon@gmail.com>
2020-06-02 00:08:39 +0900

    LeetCode 1441 - Build an Array With Stack Operations 문제 풀이 리팩토링 by Python 3

--- leetcode/python 3/1441.py
+++ leetcode/python 3/1441.py
@@ -2,14 +2,9 @@
 
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
-        l = []
         operations = []
-        for i in range(1, n + 1):
-            l.append(i)
+        for i in range(1, target[-1] + 1):
             operations.append('Push')
-            if l == target:
-                break
             if i not in target:
-                l.pop()
                 operations.append('Pop')
         return operations
