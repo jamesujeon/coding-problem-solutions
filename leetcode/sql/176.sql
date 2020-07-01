9e46a1d8285505b2104f9308bb56d2ea7aa6db66
jamesujeon <jamesujeon@gmail.com>
2020-07-01 20:55:44 +0900

    LeetCode 176 - Second Highest Salary 문제 풀이 by SQL

---
+++ leetcode/sql/176.sql
@@ -0,0 +1,8 @@
+-- 문제 링크: https://leetcode.com/problems/second-highest-salary/
+
+SELECT MAX(Salary) AS SecondHighestSalary
+FROM Employee
+WHERE Salary < (
+    SELECT MAX(Salary)
+    FROM Employee
+)
