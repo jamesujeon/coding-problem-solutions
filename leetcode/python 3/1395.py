eefb72c8624af6605e8cf2302c3981e93aee1d91
jamesujeon <jamesujeon@gmail.com>
2020-07-04 17:49:41 +0900

    LeetCode 1395 - Count Number of Teams 문제 풀이 by Python 3

---
+++ leetcode/python 3/1395.py
@@ -0,0 +1,10 @@
+# 문제 링크: https://leetcode.com/problems/count-number-of-teams/
+
+class Solution:
+    def numTeams(self, rating: List[int]) -> int:
+        count, n = 0, len(rating)
+        for j in range(1, n - 1):
+            lt = sum(rating[i] < rating[j] for i in range(j))
+            gt = sum(rating[j] < rating[k] for k in range(j + 1, n))
+            count += lt * gt + (j - lt) * (n - j - gt - 1)
+        return count
