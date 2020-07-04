0648b25e769f3a7781d268b37d8b4e21a51a215e
jamesujeon <jamesujeon@gmail.com>
2020-07-04 18:26:51 +0900

    LeetCode 538 - Convert BST to Greater Tree 문제 풀이 by Python 3

---
+++ leetcode/python 3/538.py
@@ -0,0 +1,20 @@
+# 문제 링크: https://leetcode.com/problems/convert-bst-to-greater-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        self.sum = 0
+        def dfs(root: TreeNode):
+            if not root:
+                return
+            dfs(root.right)
+            root.val = self.sum = self.sum + root.val
+            dfs(root.left)
+
+        dfs(root)
+        return root
