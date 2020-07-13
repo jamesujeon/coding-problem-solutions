3932a36ecc8f8b53d3858b5a818cb6b3f1e49cf3
jamesujeon <jamesujeon@gmail.com>
2020-07-13 19:05:55 +0900

    LeetCode 654 - Maximum Binary Tree 문제 풀이 by Python 3

---
+++ leetcode/python 3/654.py
@@ -0,0 +1,18 @@
+# 문제 링크: https://leetcode.com/problems/maximum-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
+        if not nums:
+            return None
+
+        root = TreeNode(max(nums))
+        max_index = nums.index(root.val)
+        root.left = self.constructMaximumBinaryTree(nums[:max_index])
+        root.right = self.constructMaximumBinaryTree(nums[max_index + 1:])
+        return root
