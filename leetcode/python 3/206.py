bb34707b3772ce9ded8311d5f827790ea0a5ea80
jamesujeon <jamesujeon@gmail.com>
2020-08-29 23:18:22 +0900

    LeetCode 206 - Reverse Linked List 문제 풀이 by Python 3

---
+++ leetcode/python 3/206.py
@@ -0,0 +1,15 @@
+# 문제 링크: https://leetcode.com/problems/reverse-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, current = None, head
+        while current:
+            next = current.next
+            current.next = prev
+            prev, current = current, next
+        return prev
