e9118a9303a643e5c99ea41289fbab4b3c575b23
jamesujeon <jamesujeon@gmail.com>
2020-09-01 22:04:31 +0900

    LeetCode 283 - Move Zeroes 문제 풀이 by Python 3

---
+++ leetcode/python 3/283.py
@@ -0,0 +1,8 @@
+# 문제 링크: https://leetcode.com/problems/move-zeroes/
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort(key=lambda x: 0 if x else 1)
