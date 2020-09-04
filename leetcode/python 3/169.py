fa42a3e137a5480f5db78d8c0aadbba82a04bc58
jamesujeon <jamesujeon@gmail.com>
2020-09-04 18:55:59 +0900

    LeetCode 169 - Majority Element 문제 풀이 by Python 3

---
+++ leetcode/python 3/169.py
@@ -0,0 +1,5 @@
+# 문제 링크: https://leetcode.com/problems/majority-element/
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        return collections.Counter(nums).most_common(1)[0][0]
