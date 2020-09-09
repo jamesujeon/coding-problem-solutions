4186b54c86ba21d1bf5c160410e8bc3a20212e5b
jamesujeon <jamesujeon@gmail.com>
2020-09-09 21:33:34 +0900

    LeetCode 215 - Kth Largest Element in an Array 문제 풀이 by Python 3

---
+++ leetcode/python 3/215.py
@@ -0,0 +1,5 @@
+# 문제 링크: https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return sorted(nums)[-k]
