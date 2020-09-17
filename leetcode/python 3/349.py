0279e003aafcf9ea5976555169e46f00c514dc9b
jamesujeon <jamesujeon@gmail.com>
2020-09-17 22:46:23 +0900

    LeetCode 349 - Intersection of Two Arrays 문제 풀이 by Python 3

---
+++ leetcode/python 3/349.py
@@ -0,0 +1,5 @@
+# 문제 링크: https://leetcode.com/problems/intersection-of-two-arrays/
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return set(nums1) & set(nums2)
