c2915f4edc720b8a66dacf9e30b47c08758d37ca
James M. Jeon <jamesujeon@gmail.com>
2020-09-19 23:09:37 +0900

    Delete 1507.py

--- leetcode/python 3/1507.py
+++
@@ -1,7 +0,0 @@
-# 문제 링크: https://leetcode.com/problems/reformat-date/
-
-class Solution:
-    def reformatDate(self, date: str) -> str:
-        months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        date = date.split()
-        return f'{date[2]}-{str(months.index(date[1]) + 1).zfill(2)}-{date[0][:-2].zfill(2)}'
