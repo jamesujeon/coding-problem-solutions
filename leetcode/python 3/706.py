fe093c8521c61d8433aa9324a397e30f772eac33
jamesujeon <jamesujeon@gmail.com>
2020-10-05 22:45:13 +0900

    LeetCode 706 - Design HashMap 문제 풀이 리팩토링 by Python 3

--- leetcode/python 3/706.py
+++ leetcode/python 3/706.py
@@ -1,23 +1,55 @@
 # 문제 링크: https://leetcode.com/problems/design-hashmap/
 
+class Node:
+
+    def __init__(self, key: int, value: int):
+        self.key = key
+        self.value = value
+        self.next = None
+
 class MyHashMap:
 
-    def __init__(self, capacity: int = 1000):
+    def __init__(self, capacity: int = 10000):
         self.capacity = capacity
-        self.buckets = [{}] * capacity
+        self.buckets = [None] * capacity
 
     def put(self, key: int, value: int) -> None:
-        bucket = self.buckets[self.get_hash(key)]
-        bucket[key] = value
+        hash_key = self.get_hash(key)
+        prev_bucket = bucket = self.buckets[hash_key]
+        while bucket:
+            if bucket.key == key:
+                bucket.value = value
+                return
+            prev_bucket, bucket = bucket, bucket.next
+
+        if prev_bucket:
+            prev_bucket.next = Node(key, value)
+        else:
+            self.buckets[hash_key] = Node(key, value)
 
     def get(self, key: int) -> int:
         bucket = self.buckets[self.get_hash(key)]
-        return bucket[key] if key in bucket else -1
+        while bucket:
+            if bucket.key == key:
+                return bucket.value
+            bucket = bucket.next
+        return -1
 
     def remove(self, key: int) -> None:
-        bucket = self.buckets[self.get_hash(key)]
-        if key in bucket:
-            del bucket[key]
+        hash_key = self.get_hash(key)
+        bucket = self.buckets[hash_key]
+        if not bucket:
+            return
+
+        if bucket.key == key:
+            self.buckets[hash_key] = bucket.next
+        else:
+            prev_bucket, bucket = bucket, bucket.next
+            while bucket:
+                if bucket.key == key:
+                    prev_bucket.next = bucket.next
+                    return
+                prev_bucket, bucket = bucket, bucket.next
 
     def get_hash(self, key: int) -> int:
         return key % self.capacity
