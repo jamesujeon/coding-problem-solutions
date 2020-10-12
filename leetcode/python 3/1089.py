cb94cf393b905be2d5f0fb97b19941bfda8f9a24
jamesujeon <jamesujeon@gmail.com>
2020-10-12 20:01:17 +0900

    LeetCode 1089 - Duplicate Zeros 문제 풀이 by Python 3

---
+++ leetcode/python 3/1089.py
@@ -0,0 +1,11 @@
+# 문제 링크: https://leetcode.com/problems/duplicate-zeros/
+
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        i = 0
+        while i < len(arr):
+            if arr[i] == 0:
+                arr.insert(i, 0)
+                arr.pop()
+                i += 1
+            i += 1
