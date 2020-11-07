84d03ffb4fb744f3afa34cfa7b845d652303fb31
jamesujeon <jamesujeon@gmail.com>
2020-11-07 23:22:00 +0900

    LeetCode 88 - Merge Sorted Array 문제 풀이 by Python 3

---
+++ leetcode/python 3/88.py
@@ -0,0 +1,9 @@
+# 문제 링크: https://leetcode.com/problems/merge-sorted-array/
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[m:] = nums2
+        nums1.sort()
