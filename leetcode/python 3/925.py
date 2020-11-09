afe67fe8cb5df650b8d9c5cbefb3f6634db351e6
jamesujeon <jamesujeon@gmail.com>
2020-11-09 23:36:55 +0900

    LeetCode 925 - Long Pressed Name 문제 풀이 by Python 3

---
+++ leetcode/python 3/925.py
@@ -0,0 +1,11 @@
+# 문제 링크: https://leetcode.com/problems/long-pressed-name/
+
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        i = 0
+        for j in range(len(typed)):
+            if i < len(name) and name[i] == typed[j]:
+                i += 1
+            elif i == 0 or name[i - 1] != typed[j]:
+                return False
+        return i == len(name)
