c066b43f3160b6fd1c63cf57dedc3d12b8a3ac92
jamesujeon <jamesujeon@gmail.com>
2020-11-23 23:23:23 +0900

    LeetCode 405 - Convert a Number to Hexadecimal 문제 풀이 by Python 3

---
+++ leetcode/python 3/405.py
@@ -0,0 +1,13 @@
+# 문제 링크: https://leetcode.com/problems/convert-a-number-to-hexadecimal/
+
+class Solution:
+    def toHex(self, num: int) -> str:
+        if num < 0:
+            num += 2**32
+
+        hexadecimal = "0123456789abcdef"
+        hex_num = ""
+        while num:
+            hex_num = hexadecimal[num % 16] + hex_num
+            num //= 16
+        return hex_num if hex_num else "0"
