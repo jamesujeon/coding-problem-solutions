61a31d047767f6c106e75a7ddbaccec9b1ba5961
jamesujeon <jamesujeon@gmail.com>
2020-12-18 23:18:45 +0900

    LeetCode 1688 - Count of Matches in Tournament 문제 풀이 by Python 3

---
+++ leetcode/python 3/1688.py
@@ -0,0 +1,13 @@
+# 문제 링크: https://leetcode.com/problems/count-of-matches-in-tournament/
+
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        matches = 0
+        while n > 1:
+            if n % 2 == 0:
+                n = n // 2
+                matches += n
+            else:
+                n = (n + 1) // 2
+                matches += n - 1
+        return matches
