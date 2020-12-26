eb2cfd0716f718694e5e4271200a23be6527cf6c
jamesujeon <jamesujeon@gmail.com>
2020-12-26 23:50:57 +0900

    LeetCode 888 - Fair Candy Swap 문제 풀이 by Python 3

---
+++ leetcode/python 3/888.py
@@ -0,0 +1,8 @@
+# 문제 링크: https://leetcode.com/problems/fair-candy-swap/
+
+class Solution:
+    def fairCandySwap(self, A: List[int], B: List[int]) -> List[int]:
+        swap_diff, B = (sum(A) - sum(B)) // 2, set(B)
+        for a in A:
+            if a - swap_diff in B:
+                return [a, a - swap_diff]
