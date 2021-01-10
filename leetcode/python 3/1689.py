58430bac57c0698adced89b53c252cfe488f50d9
jamesujeon <jamesujeon@gmail.com>
2021-01-10 19:18:05 +0900

    LeetCode 1689 - Partitioning Into Minimum Number Of Deci-Binary Numbers 문제 풀이 by Python 3

---
+++ leetcode/python 3/1689.py
@@ -0,0 +1,5 @@
+# 문제 링크: https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
+
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return int(max(n))
