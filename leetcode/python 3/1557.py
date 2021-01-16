a720bb6e0fc8bd672f2146a92a3ca5fcef45c456
jamesujeon <jamesujeon@gmail.com>
2021-01-16 16:29:57 +0900

    LeetCode 1557 - Minimum Number of Vertices to Reach All Nodes 문제 풀이 by Python 3

---
+++ leetcode/python 3/1557.py
@@ -0,0 +1,5 @@
+# 문제 링크: https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/
+
+class Solution:
+    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        return set(range(n)) - set(to for _, to in edges)
