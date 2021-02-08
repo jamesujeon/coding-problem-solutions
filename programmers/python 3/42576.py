677a21300fae42a2cb90a93c0e108b55bef4a10e
jamesujeon <jamesujeon@gmail.com>
2021-02-09 00:39:45 +0900

    프로그래머스 42576 - 완주하지 못한 선수 by Python 3

---
+++ programmers/python 3/42576.py
@@ -0,0 +1,6 @@
+# 문제 링크: https://programmers.co.kr/learn/courses/30/lessons/42576
+
+from collections import Counter
+
+def solution(participant, completion):
+    return list(Counter(participant) - Counter(completion))[0]
