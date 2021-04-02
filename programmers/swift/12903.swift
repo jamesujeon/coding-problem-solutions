68c437a13e3c06b231995bcf0bba7921767bf40f
jamesujeon <jamesujeon@gmail.com>
2021-04-02 18:44:44 +0900

    프로그래머스 12903 - 가운데 글자 가져오기 by Swift

--- programmers/swift/12903.swift
+++ programmers/swift/12903.swift
@@ -1,4 +1,4 @@
-// 문제 링크: https://programmers.co.kr/learn/courses/30/lessons/12919
+// 문제 링크: https://programmers.co.kr/learn/courses/30/lessons/12903
 
 func solution(_ s:String) -> String {
     let startIndex = String.Index(utf16Offset: (s.count - 1) / 2, in: s)
