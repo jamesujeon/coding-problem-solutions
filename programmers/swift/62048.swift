dcc8b2fcc1fcbfdc8b625882e699403db4ad46ca
jamesujeon <jamesujeon@gmail.com>
2021-05-15 21:43:24 +0900

    프로그래머스 62048 - 멀쩡한 사각형 by Swift

--- programmers/swift/62048.swift
+++ programmers/swift/62048.swift
@@ -1,4 +1,4 @@
-// 문제 링크: https://programmers.co.kr/learn/courses/30/lessons/12937
+// 문제 링크: https://programmers.co.kr/learn/courses/30/lessons/62048
 
 func solution(_ w: Int, _ h: Int) -> Int64 {
     Int64(w * h) - Int64(w + h - gcd(w, h))
