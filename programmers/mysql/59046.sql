56e547489d48046013ac88e43bcf8d2116883e94
Jamesu <jamesujeon@gmail.com>
2021-06-27 22:08:36 +0900

    프로그래머스 59046 - 루시와 엘라 찾기 by MySQL

---
+++ programmers/mysql/59046.sql
@@ -0,0 +1,6 @@
+-- 문제 링크: https://programmers.co.kr/learn/courses/30/lessons/59046
+
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY ANIMAL_ID ASC
