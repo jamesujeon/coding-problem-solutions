ad16d271ed70d88ac8c9b58c547708f879444acf
Jamesu <jamesujeon@gmail.com>
2021-07-15 13:26:28 +0900

    프로그래머스 81301 - 숫자 문자열과 영단어 by Python 3

---
+++ programmers/python 3/81301.py
@@ -0,0 +1,9 @@
+# 문제 링크: https://programmers.co.kr/learn/courses/30/lessons/81301
+
+def solution(s):
+    words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    for i in range(len(words)):
+        s = s.replace(words[i], str(i))
+
+    return int(s)
