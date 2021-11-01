0afb72207e3b32ad5f6c1ef2c77e5e2d14681095
Jamesu <jamesujeon@gmail.com>
2021-11-01 11:42:20 +0900

    LeetCode 27 - Remove Element by Python 3

---
+++ leetcode/python 3/27.py
@@ -0,0 +1,8 @@
+# 문제 링크: https://leetcode.com/problems/remove-element/
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        while val in nums:
+            nums.remove(val)
+
+        return len(nums)
