555b57355433a300e165bcb76b823810de73775a
Jamesu <jamesujeon@gmail.com>
2021-11-05 14:54:43 +0900

    LeetCode 70 - Climbing Stairs by Python 3

--- leetcode/python 3/70.py
+++ leetcode/python 3/70.py
@@ -3,7 +3,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         dp = [0, 1, 2] + [0] * (n - 2)
-        for i in range(3, max(n + 1, 3)):
+        for i in range(3, n + 1):
             dp[i] = dp[i - 1] + dp[i - 2]
 
         return dp[n]
