8c453c9d6401c035cdd6c441ee002598fa06f98d
Jamesu <jamesujeon@gmail.com>
2021-12-07 14:41:27 +0900

    LeetCode 495 - Teemo Attacking by Python 3

---
+++ leetcode/python 3/495.py
@@ -0,0 +1,5 @@
+# 문제 링크: https://leetcode.com/problems/teemo-attacking/
+
+class Solution:
+    def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
+        return sum(min(timeSeries[i + 1] - timeSeries[i], duration) for i in range(len(timeSeries) - 1)) + duration
