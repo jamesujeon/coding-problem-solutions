f69b8f8c1db8acb7fef2a2e2ae1740f4afe5cae4
Jamesu <jamesujeon@gmail.com>
2022-01-01 17:48:57 +0900

    LeetCode 976 - Largest Perimeter Triangle by Python 3

---
+++ leetcode/python 3/976.py
@@ -0,0 +1,11 @@
+# 문제 링크: https://leetcode.com/problems/largest-perimeter-triangle/
+
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+
+        for i in range(len(nums) - 2):
+            if nums[i] < nums[i + 1] + nums[i + 2]:
+                return sum(nums[i:i + 3])
+
+        return 0
