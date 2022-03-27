618997685b76e70264d9b943c7c9619c940587ff
Jamesu <jamesujeon@gmail.com>
2022-03-27 20:39:46 +0900

    백준 2839 - 설탕 배달 by Python 3

--- baekjoon/python 3/2839.py
+++ baekjoon/python 3/2839.py
@@ -3,10 +3,10 @@
 n = int(input())
 
 count = -1
-for x in reversed(range(n // 5 + 1)):
-  y = n - (5 * x)
-  if y >= 0 and y % 3 == 0:
-    count = x + int(y / 3)
-    break
+for five_bags in reversed(range(n // 5 + 1)):
+    m = n - (5 * five_bags)
+    if m % 3 == 0:
+        count = five_bags + m // 3
+        break
 
 print(count)
