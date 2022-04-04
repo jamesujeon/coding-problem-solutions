a2e830571f0d55bdb33d4be24b0c283fdd0945a2
Jamesu <jamesujeon@gmail.com>
2022-04-04 13:03:57 +0900

    백준 2445 - 별 찍기 - 8 by Python 3

--- baekjoon/python 3/2445.py
+++ baekjoon/python 3/2445.py
@@ -2,7 +2,5 @@
 
 N = int(input())
 
-for i in range(1, N + 1):
-    print('*' * i + ' ' * (N - i) * 2 + '*' * i)
-for i in range(1, N):
-    print('*' * (N - i) + ' ' * i * 2 + '*' * (N - i))
+for i in range(-N + 1, N):
+    print('*' * (N - abs(i)) + ' ' * abs(i) * 2 + '*' * (N - abs(i)))
