ddf3d0a188414457783d88fa1454780290ebf370
Jamesu <jamesujeon@gmail.com>
2022-04-30 13:57:45 +0900

    백준 8723 - Patyki by Python 3

---
+++ baekjoon/python 3/8723.py
@@ -0,0 +1,10 @@
+# 문제 링크: https://www.acmicpc.net/problem/8723
+
+a, b, c = sorted(map(int, input().split()))
+
+if a == b == c:
+    print(2)
+elif a**2 + b**2 == c**2:
+    print(1)
+else:
+    print(0)
