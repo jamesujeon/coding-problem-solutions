9c00d275f815fc4a37778f05e0e9d233ef5c2c5d
Jamesu <jamesujeon@gmail.com>
2022-06-28 14:09:54 +0900

    백준 18408 - 3 つの整数 (Three Integers) by Python 3

--- baekjoon/python 3/18408.py
+++ baekjoon/python 3/18408.py
@@ -2,13 +2,4 @@
 
 A, B, C = map(int, input().split())
 
-if A == B or B == C:
-    print(B)
-else:
-    print(C)
-
-# counts = {1: 0, 2: 0}
-# for i in [A, B, C]:
-#     counts[i] += 1
-
-# print(1 if counts[1] > counts[2] else 2)
+print(B if A == B or B == C else C)
