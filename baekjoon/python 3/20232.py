dd89cbee32ce8725a309d10de46777050ed6ae37
Jamesu <jamesujeon@gmail.com>
2022-07-05 16:51:19 +0900

    백준 20232 - Archivist by Python 3

---
+++ baekjoon/python 3/20232.py
@@ -0,0 +1,10 @@
+# 문제 링크: https://www.acmicpc.net/problem/20232
+
+y = int(input())
+
+if y == 2006:
+    print('PetrSU, ITMO')
+elif y in (1996, 1997, 2000, 2007, 2008, 2013, 2018):
+    print('SPbSU')
+else:
+    print('ITMO')
