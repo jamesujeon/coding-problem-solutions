38d13bef49eff6b72c7c9eb2fbb78662452cd075
Jamesu <jamesujeon@gmail.com>
2022-12-03 20:56:39 +0900

    백준 9501 - 꿍의 우주여행 by Python 3

---
+++ baekjoon/python 3/9501.py
@@ -0,0 +1,15 @@
+# 문제 링크: https://www.acmicpc.net/problem/9501
+
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    N, D = map(int, input().split())
+
+    count = 0
+    for _ in range(N):
+        v, f, c = map(int, input().split())
+        if f / c * v >= D:
+            count += 1
+
+    print(count)
