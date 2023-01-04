9409bd48f6cc715fd3f692c2f51ab4fa6c35547b
Jamesu <jamesujeon@gmail.com>
2023-01-04 22:39:00 +0900

    백준 10902 - Penalty calculation by Python 3

--- baekjoon/python 3/10902.py
+++ baekjoon/python 3/10902.py
@@ -1,11 +1,10 @@
 # 문제 링크: https://www.acmicpc.net/problem/10902
 
-f = P = max_s = 0
+max_s = P = 0
 for i in range(1, int(input()) + 1):
     t, s = map(int, input().split())
     if s > max_s:
-        f = i
-        P = t + (f - 1) * 20 if s > 0 else 0
         max_s = s
+        P = t + (i - 1) * 20 if s > 0 else 0
 
 print(P)
