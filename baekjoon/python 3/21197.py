52fa53fca42a8eab38a0d9e96e72add6b275c746
Jamesu <jamesujeon@gmail.com>
2023-04-16 17:10:04 +0900

    백준 21197 - Stopwatch by Python 3

---
+++ baekjoon/python 3/21197.py
@@ -0,0 +1,11 @@
+# 문제 링크: https://www.acmicpc.net/problem/21185
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+if N % 2 == 0:
+    t = [int(input()) for _ in range(N)]
+    print(sum(t[i + 1] - t[i] for i in range(0, N, 2)))
+else:
+    print("still running")
